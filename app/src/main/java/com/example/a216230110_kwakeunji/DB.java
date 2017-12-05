7d7226111bf7fcdb80662a39f717b61a3ff87ff7
eunjizzang97 <eunjizzang97@gmail.com>
2017-12-06 01:16:46 +0900

    "1206 AM 1:16 third commit"

--- app/src/main/java/com/example/a216230110_kwakeunji/DB.java
+++ app/src/main/java/com/example/a216230110_kwakeunji/DB.java
@@ -1,22 +1,87 @@
 package com.example.a216230110_kwakeunji;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-/**
- * Created by 곽은지 on 2017-12-05.
- */
+public class DB extends Activity {
 
-public class DB extends Activity{
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db);
 
-        String activityName = getClass().getSimpleName();
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        quantityBox =
+                (EditText) findViewById(R.id.productQuantity);
+    }
 
-        TextView tv = new TextView(this);
-        tv.setText(activityName + " 화면");
-        setContentView(tv);
+    public void newProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+        int quantity =
+                Integer.parseInt(quantityBox.getText().toString());
+        Product product =
+                new Product(productBox.getText().toString(), quantity);
+        dbHandler.addProduct(product);
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null,
+                null, 1);
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+        if (result)
+        {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
     }
 }
+
+//package com.example.a216230110_kwakeunji;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+///**
+// * Created by 곽은지 on 2017-12-05.
+// */
+//
+//public class DB extends Activity{
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//
+//        String activityName = getClass().getSimpleName();
+//
+//        TextView tv = new TextView(this);
+//        tv.setText(activityName + " 화면");
+//        setContentView(tv);
+//    }
+//}
