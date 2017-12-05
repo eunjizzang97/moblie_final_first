a91b72ed243879622fca03c00c04c91daca9a973
eunjizzang97 <eunjizzang97@gmail.com>
2017-12-05 23:55:46 +0900

    "1205 PM 11:54 first commit"

---
+++ app/src/main/java/com/example/a216230110_kwakeunji/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.a216230110_kwakeunji;
+
+import android.annotation.SuppressLint;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost mTab = getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+
+        // 첫 번째 탭
+        intent = new Intent(this, Thread.class);
+        spec = mTab.newTabSpec("Thread").setIndicator("Thread").setContent(intent);
+        mTab.addTab(spec);
+
+        // 두 번째 탭
+        intent = new Intent(this, DB.class);
+        spec = mTab.newTabSpec("DB").setIndicator("DB").setContent(intent);
+        mTab.addTab(spec);
+
+        // 세 번째 탭
+        intent = new Intent(this, Map.class);
+        spec = mTab.newTabSpec("Map").setIndicator("Map").setContent(intent);
+        mTab.addTab(spec);
+
+        // 네 번째 탭
+        intent = new Intent(this, Developer.class);
+        spec = mTab.newTabSpec("Developer").setIndicator("Dev").setContent(intent);
+        mTab.addTab(spec);
+    }
+
+    @SuppressLint("ResourceType")
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.layout.activity_main, menu);
+        return true;
+    }
+}
+
+
+//package com.example.a216230110_kwakeunji;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
---
+++ app/src/main/java/com/example/a216230110_kwakeunji/Thread.java
@@ -0,0 +1,34 @@
+package com.example.a216230110_kwakeunji;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by 곽은지 on 2017-12-05.
+ */
+
+public class Thread extends Activity{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        String activityName = getClass().getSimpleName();
+
+        TextView tv = new TextView(this);
+        tv.setText(activityName + " 화면");
+        setContentView(tv);
+    }
+}
+
+
+//package com.example.a216230110_kwakeunji;
+//
+//import android.app.Activity;
+//
+///**
+// * Created by 곽은지 on 2017-12-05.
+// */
+//
+//public class Thread extends Activity{
+//}
